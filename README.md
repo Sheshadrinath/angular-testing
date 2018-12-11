af802a928ed63efb3ceb79c847703048fbc6f07c
Sheshadrinath R <sheshadrinathr@Sheshadrinaths-MacBook-Pro.local>
2018-12-10 22:27:41 -0500

    New changes for rxjs added

--- README.md
+++ README.md
@@ -25,3 +25,18 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+
+#Basic fundamentals of RxJS
+##Observables
+It can be anything that needs to be observed. It may be a service call, value to be returned from a function or even a simple event handler
+##Observers
+These are the entities who are watching the Observables. These are nothing but a simple observer interface which consist of following methods:
+    - next()    : Pushing next value in the data stream
+    - error()   : Call error if there is any
+    - complete(): Complete the operation
+##Subscribers
+These are the parties who would like to receive the values from Observables. This is also a kindo of Observers since it implements Observer interface.
+
+#NOTES
+1. Observables are not executed until a object subscribes to it.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { PostsService } from './posts/posts.service';
 import { PostsComponent } from './posts/posts.component';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CommentsComponent } from './comments/comments.component';
+import { UsersComponent } from './users/users.component';
+
+const routes: Routes = [
+  {
+    path: 'users',
+    component: UsersComponent
+  }, 
+  {
+    path: 'posts',
+    component: PostsComponent 
+  },
+  {
+    path: '',
+    redirectTo: 'users',
+    pathMatch: 'full'
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
-    CommentsComponent
+    CommentsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     PostsService
@@ -24,4 +43,5 @@ import { CommentsComponent } from './comments/comments.component';
     AppComponent
   ]
 })
+
 export class AppModule { }
